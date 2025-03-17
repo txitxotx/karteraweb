9fcdc632301cd1deb3899c2bf1b1138391846332
txitxotx <enerux@hotmail.com>
2025-03-17 14:48:32 +0100

    Update bank.js

--- pages/bank.js
+++ pages/bank.js
@@ -3,8 +3,11 @@ import BankAccountsTable from '../components/BankAccountsTable';
 
 export default function Banks() {
   return (
-    <div>
-      <BankAccountsTable />
+    <div className="min-h-screen bg-gray-100 p-8">
+      <h1 className="text-4xl font-bold text-gray-800 text-center mb-6">Cuentas Bancarias</h1>
+      <div className="max-w-4xl mx-auto">
+        <BankAccountsTable />
+      </div>
     </div>
   );
 }
