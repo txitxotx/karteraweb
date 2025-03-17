f47352b9feab2c8bd6340ec2e55088bc52ee3fa6
txitxotx <enerux@hotmail.com>
2025-03-17 14:53:22 +0100

    Update investments.js

--- pages/investments.js
+++ pages/investments.js
@@ -3,8 +3,11 @@ import InvestmentTable from '../components/InvestmentTable';
 
 export default function Investments() {
   return (
-    <div>
-      <InvestmentTable />
+    <div className="min-h-screen bg-gray-100 p-8">
+      <h1 className="text-4xl font-bold text-gray-800 text-center mb-6">Inversiones Detalladas</h1>
+      <div className="max-w-5xl mx-auto bg-white rounded-lg shadow-lg p-6">
+        <InvestmentTable />
+      </div>
     </div>
   );
 }
