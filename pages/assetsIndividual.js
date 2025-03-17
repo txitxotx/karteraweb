38043a29bc34c8016fcd42850eb8c21e44b20797
txitxotx <enerux@hotmail.com>
2025-03-17 14:54:06 +0100

    Update assetsIndividual.js

--- pages/assetsIndividual.js
+++ pages/assetsIndividual.js
@@ -3,8 +3,11 @@ import AssetsIndividual from '../components/AssetsIndividual';
 
 export default function AssetsIndividualPage() {
   return (
-    <div>
-      <AssetsIndividual />
+    <div className="min-h-screen bg-gray-100 p-8">
+      <h1 className="text-4xl font-bold text-gray-800 text-center mb-6">Activos Individuales</h1>
+      <div className="max-w-5xl mx-auto bg-white rounded-lg shadow-lg p-6">
+        <AssetsIndividual />
+      </div>
     </div>
   );
 }
