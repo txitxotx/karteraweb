8dde4ae0b245a3c21462731fcb1cf1ba12cfbb94
txitxotx <enerux@hotmail.com>
2025-03-17 14:31:35 +0100

    Update assetsByType.js

--- pages/assetsByType.js
+++ pages/assetsByType.js
@@ -1,10 +1,8 @@
-// /pages/assetsByType.js
-import AssetsByType from '../components/AssetsByType';
+import dynamic from 'next/dynamic';
+
+// Importa el componente de forma dinámica y desactiva el renderizado en el servidor
+const AssetsByType = dynamic(() => import('../components/AssetsByType'), { ssr: false });
 
 export default function AssetsByTypePage() {
-  return (
-    <div>
-      <AssetsByType />
-    </div>
-  );
+  return <AssetsByType />;
 }
